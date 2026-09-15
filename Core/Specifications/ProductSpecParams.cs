fc56a4c1dc3d264acf0e1f2872a1caa68f5b2d23
Francois LEIGNEL <francois.leignel@arteliagroup.com>
2026-09-15 21:04:10 +0200

    End of section 8

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -7,7 +7,7 @@ public class ProductSpecParams
     private const int MaxPageSize = 50;
     public int PageIndex { get; set; } = 1;
 
-    private int _pageSize = 7;
+    private int _pageSize = 6;
     public int PageSize
     {
         get => _pageSize;
