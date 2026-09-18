0a7792b3827f9a8f94ff694224e75aa881d7e4e1
frakasmeWin <frleignel@gmail.com>
2026-09-18 12:12:29 +0200

    End of section 13

---
+++ client/src/app/shared/models/cart.ts
@@ -0,0 +1,21 @@
+import {nanoid} from 'nanoid';
+
+export type CartType = {
+    id: String;
+    items: CartItem[];
+}
+
+export type CartItem = {
+    productId: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+}
+
+export class Cart implements CartType {
+    id = nanoid();
+    items: CartItem[] = [];
+}
